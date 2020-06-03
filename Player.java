0abf55963e38abc74dbe9746bf25972fa4033d42
bryanjiang117 <66335098+bryanjiang117@users.noreply.github.com>
2020-06-03 16:54:31 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Player{
 	
 	private int gold, xp, level, health, levelUp;
+	private ArrayList<Integer> champs = new ArrayList<>();
+	boolean inferno, glacial, tank, blademaster, mage, void1, brawler; //need to add a get traits method or smth
 
 	public Player(){
 		gold = 0;
@@ -8,6 +12,28 @@ public class Player{
 		level = 1;
 		health = 100;
 		levelUp = 4;
+	}
+	
+	public void addChamp(int champ){
+		champs.add(champ);
+	}
+	
+	public void removeChamp(int champ){
+		for (int i=0; i<champs.size(); i++){
+			if (champs.get(i)==champ){
+				champs.remove(i);
+				break;
+			}
+		}
+	}
+	
+	public int getChamp(int index){
+		if (index<=champs.size()-1) return champs.get(i);
+		return -1;
+	}
+	
+	public int getNChamps(){
+		return champs.size();
 	}
 	
 	public void gainGold(int gold){

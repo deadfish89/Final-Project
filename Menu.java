da797a8b66cae42de9ae117f8f009c1baf6f72be
bryanjiang117 <66335098+bryanjiang117@users.noreply.github.com>
2020-06-13 11:10:47 -0400

    put set size in menu instead of frame

--- Menu.java
+++ Menu.java
@@ -1,41 +1,41 @@
-import java.awt.*; 
 import javax.swing.*;
-import java.awt.event.*; 
-import java.util.Arrays;
+import java.awt.*;
+import java.awt.event.*;
 
-public class TFT{
-
-	public static JFrame frame;
-	public static Container container;
-	
-	public static void main(String[] args){
-		frame = new JFrame("Teamfight Tactics");
-		container = frame.getContentPane();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		LoadMenu();
-	}
-	public static void LoadMenu(){
-		container.removeAll();
-		Menu menu = new Menu();
-	    container.add(menu,BorderLayout.CENTER);
-		frame.revalidate();
-		frame.repaint();
-		frame.pack();
-	}
-	public static void LoadGame(){
-		container.removeAll();
-		Player player = new Player();
-		Board board = new Board(player);
-		container.add(board, BorderLayout.CENTER);
-		container.add(new SouthPanel(player, board), BorderLayout.SOUTH);
-		frame.pack();
-	}
-	public static void LoadInstructions(){
-		container.removeAll();
-		InstructionPanel panel = new InstructionPanel();
-		container.add(panel, BorderLayout.CENTER);
-		frame.revalidate();
-		frame.repaint();
-	}
+class Menu extends JPanel implements ActionListener {
+    private JLabel title;
+    private JButton play,instruction;
+    private ImageIcon logo;
+    public Menu(){
+		this.setPreferredSize(new Dimension(1300,700));
+        logo = new ImageIcon("TitleLogo.png");
+        play = new JButton("Play");
+        instruction = new JButton("Instructions");
+        title = new JLabel(logo);
+        play.addActionListener(this);
+		play.setBackground(Color.GREEN);
+		play.setOpaque(true);
+        instruction.addActionListener(this);
+        this.setBackground(Color.ORANGE);
+        this.setLayout(null);
+        title.setBounds(0,0,1300,350);
+        play.setBounds(400,400,500,75);
+        instruction.setBounds(400,550,500,75);
+        this.add(title);
+        this.add(play);
+        this.add(instruction);
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+		g.setFont(new Font("Open Sans",Font.PLAIN,20));
+		g.drawString("By:Bryan Jiang and Eric Chen",1000,650);
+    }
+    public void actionPerformed(ActionEvent event) {
+            if(event.getSource()==play){
+		TFT.LoadGame();
+            }
+            if(event.getSource()==instruction){
+                TFT.LoadInstructions();
+            }
+    }
 }

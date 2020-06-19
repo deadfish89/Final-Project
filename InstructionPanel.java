c88a536bee2e14d18f2ad5684782f994e46b572c
deadfish89 <45515558+deadfish89@users.noreply.github.com>
2020-06-19 11:21:26 -0400

    improving readability

--- InstructionPanel.java
+++ InstructionPanel.java
@@ -3,31 +3,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 class InstructionPanel extends JPanel implements ActionListener{
+
 	private JLabel title,shopPicture,traitPicture,starPicture,bottomtext;
     	private JButton returnMenu;
+
 	public InstructionPanel(){
+
 		shopPicture = new JLabel(new ImageIcon("ShopPicture.png"));
 		traitPicture = new JLabel(new ImageIcon("TraitPicture.png"));
 		starPicture = new JLabel(new ImageIcon("StarPicture.png"));
 		returnMenu = new JButton("Return to Menu");
         	bottomtext = new JLabel("All champions and art belong to Riot Games. Pls dont sue me.");
 		returnMenu.addActionListener(this);
+
 		this.setBackground(Color.WHITE);
         	this.setLayout(null);
+
+		//dont understand how setbounds works but its fine ig
 		shopPicture.setBounds(540,0,700,300);
 		traitPicture.setBounds(100,-25,500,700);
 		starPicture.setBounds(100,400,700,300);
 		returnMenu.setBounds(1000,500,200,100);
+
 		returnMenu.setBackground(new Color(246,178,61));
 		returnMenu.setOpaque(true);
 		returnMenu.setBorder(BorderFactory.createBevelBorder(0));
 		returnMenu.setFont(new Font("SansSerif",Font.PLAIN,20));
+
 		this.add(shopPicture);
 		this.add(traitPicture);
 		this.add(starPicture);
 		this.add(returnMenu);
 	}
+
 	public void paintComponent(Graphics g){
+
         	super.paintComponent(g);
         	g.setFont(new Font("SansSerif",Font.BOLD,50));
         	g.drawString("Instructions",450,50);
@@ -36,6 +46,7 @@ class InstructionPanel extends JPanel implements ActionListener{
 		g.drawString("Each champion has traits. Having champions",500,300); 
 		g.drawString("with the same traits gives bonuses.",500,350);
 		g.drawString("Combine 3 of the same champion to make a more powerful version",50,475);
+
     	}
     	public void actionPerformed(ActionEvent event) {
             if(event.getSource()==returnMenu){
